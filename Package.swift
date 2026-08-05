2bec9d5d558bf5f9733c32c028b08f549cb21a8a
zhengrenzhe <zhengrenzhe0416@outlook.com>
2026-08-06 00:14:17 +0800

    feat: define cockpit handshake protocol

--- Package.swift
+++ Package.swift
@@ -7,10 +7,28 @@ let package = Package(
     platforms: [.macOS(.v15)],
     products: [
         .library(name: "CockpitTypes", targets: ["CockpitTypes"]),
+        .library(name: "CockpitProtocol", targets: ["CockpitProtocol"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-protobuf.git", exact: "1.38.1"),
     ],
     targets: [
         .target(name: "CockpitTypes"),
         .testTarget(name: "CockpitTypesTests", dependencies: ["CockpitTypes"]),
+        .target(
+            name: "CockpitProtocol",
+            dependencies: [
+                "CockpitTypes",
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+            ],
+            plugins: [
+                .plugin(name: "SwiftProtobufPlugin", package: "swift-protobuf"),
+            ]
+        ),
+        .testTarget(
+            name: "CockpitProtocolTests",
+            dependencies: ["CockpitProtocol"]
+        ),
     ],
     swiftLanguageModes: [.v6]
 )
